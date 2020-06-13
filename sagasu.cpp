24808868208a447dea93852e23f404b8b9b21f85
jj1guj <57407826+jj1guj@users.noreply.github.com>
2020-06-13 13:15:25 +0900

    vectorの参照渡しによる高速化

--- sagasu.cpp
+++ sagasu.cpp
@@ -1,6 +1,6 @@
 #include "dekunobou.hpp"
 
-vector<vector<int>> row_migi(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>> basyo) {
+void row_migi(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>>& basyo) {
 	int i;
     for (i = 2; i < 8 - y; i++) {
         if (row[x][y + i] == b && search_zahyou(basyo,x,y)==false) {
@@ -10,10 +10,9 @@ vector<vector<int>> row_migi(vector<vector<int>>& row, int x, int y, int b, vect
         	break;
         }
     }
-    return basyo;
 };
 
-vector<vector<int>> row_hidari(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>> basyo) {
+void row_hidari(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>>& basyo) {
     int i;
 	for (i = 2; i < y+1; i++) {
         if (row[x][y - i] == b && search_zahyou(basyo,x,y)==false) {
@@ -23,10 +22,9 @@ vector<vector<int>> row_hidari(vector<vector<int>>& row, int x, int y, int b, ve
         	break;
         }
     }
-    return basyo;
 };
 
-vector<vector<int>> clm_ue(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>> basyo) {
+void clm_ue(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>>& basyo) {
     int i;
 	for (i = 2; i < 8 - x; i++) {
         if (row[x+i][y] == b && search_zahyou(basyo,x,y)==false) {
@@ -36,10 +34,9 @@ vector<vector<int>> clm_ue(vector<vector<int>>& row, int x, int y, int b, vector
         	break;
         }
     }
-    return basyo;
 };
 
-vector<vector<int>> clm_sita(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>> basyo) {
+void clm_sita(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>>& basyo) {
 	int i;
     for (i = 2; i < x+1; i++) {
         if (row[x-i][y] == b && search_zahyou(basyo,x,y)==false) {
@@ -49,10 +46,9 @@ vector<vector<int>> clm_sita(vector<vector<int>>& row, int x, int y, int b, vect
         	break;
         }
     }
-    return basyo;
 };
 
-vector<vector<int>> miginaname_ue(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>> basyo) {
+void miginaname_ue(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>>& basyo) {
 	int i;
     for (i = 2; i < min(8-x,8-y); i++) {
         if (row[x+i][y+i] == b && search_zahyou(basyo,x,y)==false) {
@@ -62,10 +58,9 @@ vector<vector<int>> miginaname_ue(vector<vector<int>>& row, int x, int y, int b,
         	break;
         }
     }
-    return basyo;
 };
 
-vector<vector<int>> miginaname_sita(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>> basyo) {
+void miginaname_sita(vector<vector<int>>& row, int x, int y, int b, vector<vector<int>>& basyo) {
     int i;
 	for (i = 2; i < min(x+1,y+1); i++) {
         if (row[x-i][y-i] == b && search_zahyou(basyo,x,y)==false) {
@@ -75,10 +70,9 @@ vector<vector<int>> miginaname_sita(vector<vector<int>>& row, int x, int y, int 
         	break;
         }
     }
-    return basyo;
 };
 
-vector<vector<int>> hidarinaname_ue(vector<vector<int>>& row,int x,int y,int b,vector<vector<int>> basyo){
+void hidarinaname_ue(vector<vector<int>>& row,int x,int y,int b,vector<vector<int>>& basyo){
 	int i;
 	for(i=2;i<min(8-x,y+1);i++){
 		if(row[x+i][y-i]==b && search_zahyou(basyo,x,y)==false){
@@ -88,10 +82,9 @@ vector<vector<int>> hidarinaname_ue(vector<vector<int>>& row,int x,int y,int b,v
 			break;
 		}
 	}
-	return basyo;
 };
 
-vector<vector<int>> hidarinaname_sita(vector<vector<int>>& row,int x,int y,int b,vector<vector<int>> basyo){
+void hidarinaname_sita(vector<vector<int>>& row,int x,int y,int b,vector<vector<int>>& basyo){
 	int i;
 	for(i=2;i<min(x+1,8-y);i++){
 		if(row[x-i][y+i]==b && search_zahyou(basyo,x,y)==false){
@@ -101,7 +94,6 @@ vector<vector<int>> hidarinaname_sita(vector<vector<int>>& row,int x,int y,int b
 			break;
 		}
 	}
-	return basyo;
 };
 
 vector<vector<int>> basyo_hyouji(vector<vector<int>> basyo){
@@ -138,18 +130,18 @@ vector<vector<int>> sagasu(vector<vector<int>>& row, int soutesu, int sente) {
 			//行方向
 			if(j<=6){
 				if(row[i][j]==0 && row[i][j+1]==a){
-					basyo=row_migi(row,i,j,b,basyo);
+					row_migi(row,i,j,b,basyo);
 				}else if(row[i][j]==a && row[i][j+1]==0){
-					basyo=row_hidari(row,i,j+1,b,basyo);
+					row_hidari(row,i,j+1,b,basyo);
 				}
 			}
 
 			//列方向
 			if(i<=6){
 				if(row[i][j]==0 && row[i+1][j]==a){
-					basyo=clm_ue(row,i,j,b,basyo);
+					clm_ue(row,i,j,b,basyo);
 				}else if(row[i][j]==a && row[i+1][j]==0){
-					basyo=clm_sita(row,i+1,j,b,basyo);
+					clm_sita(row,i+1,j,b,basyo);
 				}
 			}
 
@@ -159,32 +151,32 @@ vector<vector<int>> sagasu(vector<vector<int>>& row, int soutesu, int sente) {
 					//盤面下部
 					//右斜め方向
 					if(row[j+k][j]==0 && row[j+k+1][j+1]==a){
-						basyo=miginaname_ue(row,j+k,j,b,basyo);
+						miginaname_ue(row,j+k,j,b,basyo);
 					}else if(row[j+k][j]==a && row[j+k+1][j+1]==0){
-						basyo=miginaname_sita(row,j+k+1,j+1,b,basyo);
+						miginaname_sita(row,j+k+1,j+1,b,basyo);
 					}
 
 					//左斜め方向
 					if(row[j+k][7-j]==0 && row[j+k+1][7-(j+1)]==a){
-						basyo=hidarinaname_ue(row,j+k,7-j,b,basyo);
+						hidarinaname_ue(row,j+k,7-j,b,basyo);
 					}else if(row[j+k][7-j]==a && row[j+k+1][7-(j+1)]==0){
-						basyo=hidarinaname_sita(row,j+k+1,7-(j+1),b,basyo);
+						hidarinaname_sita(row,j+k+1,7-(j+1),b,basyo);
 					}
 
 					//盤面上部
 					if(k!=0){
 						//右斜め方向
 						if(row[j][j+k]==0 && row[j+1][j+k+1]==a){
-							basyo=miginaname_ue(row,j,j+k,b,basyo);
+							miginaname_ue(row,j,j+k,b,basyo);
 						}else if(row[j][j+k]==a && row[j+1][j+k+1]==0){
-							basyo=miginaname_sita(row,j+1,j+k+1,b,basyo);
+							miginaname_sita(row,j+1,j+k+1,b,basyo);
 						}
 
 						//左斜め方向
 						if(row[j][7-(j+k)]==0 && row[j+1][7-(j+k+1)]==a){
-							basyo=hidarinaname_ue(row,j,7-(j+k),b,basyo);
+							hidarinaname_ue(row,j,7-(j+k),b,basyo);
 						}else if(row[j][7-(j+k)]==a && row[j+1][7-(j+k+1)]==0){
-							basyo=hidarinaname_sita(row,j+1,7-(j+k+1),b,basyo);
+							hidarinaname_sita(row,j+1,7-(j+k+1),b,basyo);
 						}
 					}
 				}
@@ -192,6 +184,6 @@ vector<vector<int>> sagasu(vector<vector<int>>& row, int soutesu, int sente) {
 		}
 	}
 
-	sort(basyo.begin(),basyo.end());
+	//sort(basyo.begin(),basyo.end());
     return basyo;
 };
